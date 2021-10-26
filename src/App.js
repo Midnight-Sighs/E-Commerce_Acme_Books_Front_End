77c745f31c3e5f08e2e74be12ea9932d2527ab56
Midnight <arreiskurai@yahoo.com>
2021-10-26 14:24:07 -0700

    Book card component basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Styles/App.css';
 import BookCard from './Components/MainShop/BookCard';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Components/MainShop/BookCard.jsx
+++ src/Components/MainShop/BookCard.jsx
@@ -5,15 +5,17 @@ import PlaceholderCover from '../../Images/PlaceholderCover.png'
 const BookCard=(props)=> {
     return ( 
         <>
-            <div>
+            <div className="container book-card">
                 <div className="row">
-                    <div className="book-card">
-                        <img className="col-2 bc-img" src={PlaceholderCover}></img>
+                    <div className="col-2 bc-part">
+                        <img className="bc-img" src={PlaceholderCover}></img>
                     </div>
-                    <div className="col-2">
+                    <div className="col-2 bc-part">
+                        <p>
                         Title{/*props.book.title*/}
                         <br />Author{/*props.book.author*/}
                         <br />Price {/*props.book.price*/}
+                        </p>
                     </div>
                     <div className="col-8"></div>
                 </div>
