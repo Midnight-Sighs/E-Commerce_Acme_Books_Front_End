6d3b05f066cb8eee93ee0020cd108d35d4f72c84
Midnight <arreiskurai@yahoo.com>
2021-10-26 10:00:52 -0700

    quick layout of the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Styles/App.css';
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-        <h1>Variable Test</h1>
+        
     </div>
   );
 }
