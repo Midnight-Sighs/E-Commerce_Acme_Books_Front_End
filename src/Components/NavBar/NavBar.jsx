1cc6d4dc68caf1042c730ef8fd06e2731b78e2a3
Midnight <arreiskurai@yahoo.com>
2021-10-27 07:59:46 -0700

    Further clean up and integration of the last merge

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,29 +3,29 @@ import '../Styles/Components.css'
 
 const NavBar = (props) =>{
 
-    const[anon, setAnon] = useState(true);
-    const[buyer, setBuyer] = useState(false);
-    const[seller, setSeller] =useState(false);
+    // const[anon, setAnon] = useState(true);
+    // const[buyer, setBuyer] = useState(false);
+    // const[seller, setSeller] =useState(false);
 
-    useEffect(()=>{
-        let currentPage = props.currentPage
-        checkPageType(currentPage)
-    }, [props])
+    // useEffect(()=>{
+    //     let currentPage = props.currentPage
+    //     checkPageType(currentPage)
+    // }, [props])
 
-    const checkPageType=(page)=>{
-        if(page.currentPage == "anon" ){
-            setAnon(true);
-        }
-        if(page.currentPage == "buyer"){
-            setBuyer(true);
-            setSeller(true);
-            setAnon(false);
-        }
-        if(page.currentPage == "seller"){
-            setSeller(true);
-            setAnon(false);
-        }
-    }
+    // const checkPageType=(page)=>{
+    //     if(page.currentPage == "anon" ){
+    //         setAnon(true);
+    //     }
+    //     if(page.currentPage == "buyer"){
+    //         setBuyer(true);
+    //         setSeller(true);
+    //         setAnon(false);
+    //     }
+    //     if(page.currentPage == "seller"){
+    //         setSeller(true);
+    //         setAnon(false);
+    //     }
+    // }
 
     return ( 
         <> 
@@ -37,12 +37,12 @@ const NavBar = (props) =>{
                 {/*Search Bar Here*/}
             </div>
             <div className="col-4 nav-links">
-                {anon ? <span>|  Login</span> :null}
+                {/* {anon ? <span>|  Login</span> :null}
                 {anon ? <span> |  Register </span>:null}
                 {seller ? <span>|  Logout</span>: null}
                 {seller ? <span>|  Edit Profile </span>: null}
                 {seller ? <span>|  Cart</span>:null}
-                {buyer ? <span>|  New Product </span>:null}
+                {buyer ? <span>|  New Product </span>:null} */}
             </div>
 
         </div>
--- src/Pages/BookDetailPage/BookDetailPage.jsx
+++ src/Pages/BookDetailPage/BookDetailPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Header from '../../Components/Header/Header'
+import NavBar from '../../Components/NavBar'
+import BookDetails from '../../Components/BookDetails/BookDetails'
 
 class BookDetailPage extends Component {
     constructor(props) {
@@ -11,6 +13,8 @@ class BookDetailPage extends Component {
         return ( 
             <>
             <Header />
+            <NavBar />
+            <BookDetails />
             </>
          );
     }
